0f4c39f015ad132fe857c54afee32663c313b342
Stephen Halter <halter73@gmail.com>
2013-10-16 15:57:46 -0700

    Added the ability to access state from a new property "CallerState"
    - This is necessary to access the state value from VB.NET
    - This makes the caller's state accessible from inside a type Hub
    
    #2620

---
+++ samples/Microsoft.AspNet.SignalR.Samples.VB/VBTestHub.vb
@@ -0,0 +1,22 @@
+﻿' SignalR
+Imports Microsoft.AspNet.SignalR
+Imports Microsoft.AspNet.SignalR.Hubs
+
+<HubName("VBDemo")>
+Public Class VbDemoHub
+    Inherits Hub
+
+    Public Overrides Function OnConnected() As Task
+        Clients.CallerState.message = "Why?"
+
+        ' Invoke a method on the client so the updated state is also sent
+        Clients.Caller.anyMethodNameWillDo()
+
+        Return MyBase.OnConnected()
+    End Function
+
+    Public Function ReadStateValue()
+        Dim z As String = Clients.CallerState.message
+        Return z
+    End Function
+End Class
